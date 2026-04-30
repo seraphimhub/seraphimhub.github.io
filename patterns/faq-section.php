257684c9b2d82302a5d0135ca6466f53db7e06ff
Infosec Tools ID <angelseraphim64@gmail.com>
2026-04-30 22:44:28 +0700

    Add files via upload

---
+++ patterns/faq-section.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Title: FAQ Section
+ * Slug: cyber-security-blocks/faq-section
+ * Categories: cyber-security-blocks, faq-section
+ */
+?>
+
+<!-- wp:group {"className":"faq-section wow fadeInUp","style":{"spacing":{"padding":{"right":"0px","left":"0px"}}},"layout":{"type":"constrained","contentSize":"75%"}} -->
+<div class="wp-block-group faq-section wow fadeInUp" style="padding-right:0px;padding-left:0px"><!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"className":"our-services-small-heading","layout":{"type":"constrained","contentSize":"15%","justifyContent":"center"}} -->
+<div class="wp-block-group our-services-small-heading"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"primary","textColor":"white","fontSize":"small"} -->
+<h3 class="wp-block-heading has-text-align-center has-white-color has-primary-background-color has-text-color has-background has-link-color has-small-font-size" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30);font-style:normal;font-weight:600;text-transform:uppercase"><?php esc_html_e('FAQS','cyber-security-blocks'); ?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"className":"faq-section-cols"} -->
+<div class="wp-block-columns faq-section-cols"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"faq-section-main-box","layout":{"type":"default"}} -->
+<div class="wp-block-group faq-section-main-box"><!-- wp:details {"showContent":true,"className":"faq-detail-block","style":{"typography":{"fontSize":"20px"},"elements":{"link":{"color":{"text":"var:preset|color|heading"}}}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px" open><summary><?php esc_html_e('What is cyber security and why is it important?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('How can I protect my personal data online?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('Do you offer 24/7 monitoring?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('Can you prevent ransomware attacks?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"faq-section-main-box","layout":{"type":"default"}} -->
+<div class="wp-block-group faq-section-main-box"><!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('Do you provide security audits?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('Can you train our staff on cyber safety?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('How quickly can you respond to threats?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('How often do you update security measures?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"className":"faq-detail-block","style":{"elements":{"link":{"color":{"text":"var:preset|color|heading"}}},"typography":{"fontSize":"20px"}},"textColor":"heading"} -->
+<details class="wp-block-details faq-detail-block has-heading-color has-text-color has-link-color" style="font-size:20px"><summary><?php esc_html_e('How do I know if a website is secure?','cyber-security-blocks'); ?></summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block","style":{"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:300"><?php esc_html_e('Consectetur adipiscing eltadip isngelitseddo eusmod tempor incididunt utlabore edolorema gnasdoeiusmodtempor incididunt utrices gravida. Consecte adipiscingelitadipiscing elitserissed.','cyber-security-blocks'); ?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
